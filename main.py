bcd237e301a32e6e8a3158c1bad7bb7260d08a96
Kamil Małkowski <kamil.malkowski96@gmail.com>
2023-01-25 21:39:01 +0100

    fixed invalid input and added functions - menu, new_character

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from character import *
 
-char_name = input("Enter name ")
+
+def main():
+    print("Hello in console-rpg, choose what you want to do")
+    print("")
+    print("New character = 1 ")
+    print("Load old character = 2")
+    while True:
+        user_chose_main = input("Enter a number (1 or 2) ")
+        if user_chose_main.isdigit():
+            user_chose_main = int(user_chose_main)
+            if user_chose_main == 1:
+                new_character()
+            if user_chose_main == 2:
+                load_character()
+            else:
+                print("Enter again a number (1 or 2)")
+        else:
+            print("Enter again a number")
+
+
+def load_character():
+    print("No elo")
+    menu()
 
 
 def exp():
@@ -11,32 +33,51 @@ def shop():
     print("You want to buy something?")
 
 
-def main():
+def menu():
     print()
     print("Choose what you want to do next")
     print("Go to shop - chose 1")
     print("Go to exp - chose 2")
-    number_in_menu = int(input("Enter a number "))
-    if number_in_menu == 1:
-        print("You chose shop")
-        shop()
-    if number_in_menu == 2:
-        print("You chose exp")
-        exp()
+    while True:
+        number_in_menu = input("Enter a number (1 or 2) ")
+        if number_in_menu.isdigit():
+            number_in_menu = int(number_in_menu)
+            if number_in_menu == 1:
+                print("You chose shop")
+                shop()
+            if number_in_menu == 2:
+                print("You chose exp")
+                exp()
+            else:
+                print("Enter again a number (1 or 2)")
+        else:
+            print("Enter again a number")
 
 
-print()
-print("Choose your character")
-print("Knight = 1")
-print("Druid = 2")
-user_chose = int(input("Enter a number "))
+def new_character():
+    print()
+    print("Here you will create a new character, but first enter a name for your character")
+    char_name = input("Enter name ")
+    print("Ok, next choose your class")
+    print("Knight = 1")
+    print("Druid = 2")
+    while True:
+        user_chose = input("Enter a number (1 or 2) ")
+        if user_chose.isdigit():
+            user_chose = int(user_chose)
+            if user_chose > 0:
+                if user_chose == 1:
+                    char_knight = Knight(char_name)
+                    print("You chose a knight, Hello", char_name)
+                    menu()
+                if user_chose == 2:
+                    char_druid = Druid(char_name)
+                    print("You chose a druid, Hello", char_name)
+                    menu()
+                else:
+                    print("Enter again a number (1 or 2)")
+        else:
+            print("Enter again a number")
 
-if user_chose > 0:
-    if user_chose == 1:
-        char_knight = Knight(char_name)
-        print("You chose a knight, Hello", char_name)
-        main()
-    if user_chose == 2:
-        char_druid = Druid(char_name)
-        print("You chose a druid, Hello", char_name)
-        main()
+
+main()
